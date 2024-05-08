d9458704d2df255865c3c2497eefac6fb79ecabd
Rufi <Rafajos9@gmail.com>
2024-05-08 11:06:49 -0400

    Added vite and tailwind

---
+++ vite.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'dist',
+    },
+    server: {
+        open: true,
+      },
+    plugins: [react(),eslint(),svgr({ svgrOptions: { icon: true } })],
+    base:'http://rufi512.github.io/CRUD-React-Redux-LocalStorage'
+  };
+});
